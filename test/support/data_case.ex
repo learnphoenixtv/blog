b693d2f9f1deefed73c98a18f97b82611b71d435
Daniel Berkompas <himself@danielberkompas.com>
2017-02-10 06:35:40 -0800

    Initial commit

---
+++ test/support/data_case.ex
@@ -0,0 +1,37 @@
+defmodule Blog.DataCase do
+  @moduledoc """
+  This module defines the setup for tests requiring
+  access to the application's data layer.
+
+  You may define functions here to be used as helpers in
+  your tests.
+
+  Finally, if the test case interacts with the database,
+  it cannot be async. For this reason, every test runs
+  inside a transaction which is reset at the beginning
+  of the test unless the test case is marked as async.
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      alias Blog.Repo
+
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query
+      import Blog.DataCase
+    end
+  end
+
+  setup tags do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Elixir.Blog.Repo)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(Elixir.Blog.Repo, {:shared, self()})
+    end
+
+    :ok
+  end
+end
